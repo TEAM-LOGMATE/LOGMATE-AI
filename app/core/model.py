2e9d70b738aabbef42f7d9cc8ac04231f80b19df
SeungWon Yang <ey8968@naver.com>
2025-08-15 16:51:33 +0900

    [Feat] seungwon: scikit-learn 경고메시지 오류 무시

--- app/core/model.py
+++ app/core/model.py
@@ -1,5 +1,8 @@
 import joblib
 import os
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="sklearn")
+
 
 def load_model(filename: str):
     full_path = os.path.join("app", "model", filename)
